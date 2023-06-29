7ccdcf1b493a0e7bc39a392764c8f988130f1676
sameichner <sameichner@gmail.com>
2023-06-29 18:11:50 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('node:path')
+
+const app = express()
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+});
+
+app.use('/static', express.static('public'));
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
+
