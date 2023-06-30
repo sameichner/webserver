ad141d31b427a826af8eb9ee2cab9149741d8dad
sameichner <sameichner@gmail.com>
2023-06-30 19:32:28 -0400

    switching to mysql2 for new authentication

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('node:path');
 const dotenv = require('dotenv');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const app = express()
 const port = 8000;
