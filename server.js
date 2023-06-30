93d66fd00085e9cd88dec1fc101c9e84f0fe73e8
sameichner <sameichner@gmail.com>
2023-06-30 18:13:11 -0400

    installing neccessary libraries

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const path = require('node:path')
+const path = require('node:path');
+const dotenv = require('dotenv');
+const mysql = require('mysql');
 
 const app = express()
 const port = 8000;
