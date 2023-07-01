4eb31fe30e66923b27b2a4a25aa2ee6b2f2775fe
sameichner <sameichner@gmail.com>
2023-06-30 23:13:03 -0400

    adding express session

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('node:path');
+const session = require('express-session')
 const dotenv = require('dotenv').config();
 const mysql = require('mysql2');
 
