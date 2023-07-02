a8db9c01164372cf78ce3959bc4ff2e473c3d388
sameichner <sameichner@gmail.com>
2023-07-01 21:11:35 -0400

    account registration system (no hash yet)

--- server.js
+++ server.js
@@ -24,7 +24,32 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/login.html'))
+});
+
 app.use('/static', express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.post('/auth/register', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    if (username && password) {
+        dbcon.query(`INSERT INTO logins VALUES (0, ?, ?)`, [username, password], 
+            (err, result) => {
+                if (err) {
+                    if ((err.code) == 'ER_DUP_ENTRY') {
+                        res.send('This username is already taken!');
+                        return;
+                    } else throw err;
+                } else {
+                    res.send('Sucessfully Registered!');
+                }
+            }
+        );
+    }
+});
 
 app.listen(port, () => {
     dbcon.connect(((err)=>{
